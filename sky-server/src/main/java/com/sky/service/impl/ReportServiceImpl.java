364a2e8a8e915bfc5d03a36d5a1807cecf4584d3
李明翰 <1623806553@qq.com>
2024-10-24 10:34:35 +0800

    销量排名

--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -2,11 +2,13 @@ package com.sky.service.impl;
 
 
 import com.alibaba.fastjson.JSON;
+import com.sky.dto.GoodsSalesDTO;
 import com.sky.entity.Orders;
 import com.sky.mapper.OrderMapper;
 import com.sky.mapper.UserMapper;
 import com.sky.service.ReportService;
 import com.sky.vo.OrderReportVO;
+import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 import org.apache.commons.lang.StringUtils;
@@ -20,6 +22,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class ReportServiceImpl implements ReportService {
@@ -148,7 +151,7 @@ public class ReportServiceImpl implements ReportService {
 //        完成率
         Double orderCompletionRate = 0.0;
         if (allTotals != 0) {
-            orderCompletionRate =  dailyTotals.doubleValue()/allTotals * 100;
+            orderCompletionRate =  dailyTotals.doubleValue()/allTotals;
         }
 
         return  OrderReportVO.builder()
@@ -158,6 +161,27 @@ public class ReportServiceImpl implements ReportService {
                 .totalOrderCount(allTotals)
                 .validOrderCount(dailyTotals)
                 .orderCompletionRate(orderCompletionRate)
+                .build();
+    }
+
+    /**
+     * 销量排名
+     * @param begin
+     * @param end
+     * @return
+     */
+    @Override
+    public SalesTop10ReportVO getSaleTop10(LocalDate begin, LocalDate end) {
+        LocalDateTime beginTime = LocalDateTime.of(begin, LocalTime.MIN);
+        LocalDateTime endTime = LocalDateTime.of(end, LocalTime.MAX);
+        List<GoodsSalesDTO> salesTop = orderMapper.getSalesTop(beginTime, endTime);
+        List<String> names = salesTop.stream().map(GoodsSalesDTO::getName).collect(Collectors.toList());
+        String nameSting = StringUtils.join(names, ",");
+        List<Integer> number = salesTop.stream().map(GoodsSalesDTO::getNumber).collect(Collectors.toList());
+        String numberString = StringUtils.join(number, ",");
+        return  SalesTop10ReportVO.builder()
+                .nameList(nameSting)
+                .numberList(numberString)
                 .build();
     }
 
